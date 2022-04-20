4622c9262e5c712dfd4fa3b3eb3f8cda9ea52acd
Amolterer <alexander.molterer@kellogg.northwestern.edu>
2022-04-20 02:57:22 +0000

    version 6

--- kmdb.sql
+++ kmdb.sql
@@ -134,9 +134,35 @@ INSERT INTO movies(
     VALUES (
     "Batman Begins",
     "2005",
-    "PG-Rating",
+    "PG-13",
     "Warner Bros."    
     );
+
+INSERT INTO movies(
+    title,
+    year,
+    MPAA_rating,
+    studio
+    )
+    VALUES (
+    "The Dark Knight",
+    "2008",
+    "PG-13",
+    "Warner Bros."    
+    );
+
+INSERT INTO movies (
+    title,
+    year,
+    MPAA_rating, 
+    studio)
+    VALUES (
+    "The Dark Knight Rises",
+    "2012",
+    "PG-13",
+    "Warner Bros."
+    ); 
+
 -- Prints a header for the movies output
 .print "Movies"
 .print "======"
